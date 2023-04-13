2cd07e319086f81f7a8dfe867d83e028554a6da9
Ilya <devdokimov.ilya@yandex.ru>
2023-04-13 23:00:09 +0300

    Базовая вёрстка (хедер + футер) и фавикон

--- views/layouts/footer.php
+++ views/layouts/footer.php
@@ -1,5 +1,13 @@
 </main>
-    <footer class="footer">footer</footer>
+    <footer class="footer">
+        <div class="container">
+            <div class="footer__inner">
+                <div class="footer__text">
+                    Разработано специально для <a href="https://github.com/AtmoSL" target="_blank">портфолио</a>
+                </div>
+            </div>
+        </div>
+    </footer>
 </div>
 </body>
 </html>
--- views/layouts/header.php
+++ views/layouts/header.php
@@ -5,9 +5,34 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>БИЛЕТИКУС</title>
+    <title>Билетикус</title>
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="/css/reset.css">
+    <link rel="stylesheet" href="/css/fonts.css">
+    <link rel="stylesheet" href="/css/main.css">
 </head>
 <body>
 <div class="wrapper">
-    <header class="header">Header</header>
+    <header class="header">
+        <div class="container">
+            <nav class="header__nav">
+                <a href="/" class="header__logo__link">
+                    <div class="header__logo">
+                        <div class="header__logo__text">
+                            Билетикус
+                        </div>
+                        <div class="header__logo__img">
+                            <img src="/img/logo.png" alt="logo" class="">
+                        </div>
+                    </div>
+                </a>
+                <ul class="header__nav__list">
+                    <li><a href="/">Билеты</a></li>
+                    <li><a href="#">Театры</a></li>
+                    <li><a href="#">Вход</a></li>
+                    <li><a href="#">Регистрация</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
     <main class="main">
