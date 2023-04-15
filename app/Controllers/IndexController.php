2c73d66cb81bcbf7afa0ed632ce11c54d5573b79
Ilya <devdokimov.ilya@yandex.ru>
2023-04-15 17:37:56 +0300

    Вывод карточек событий по тестовому массиву из контроллера

--- app/Controllers/IndexController.php
+++ app/Controllers/IndexController.php
@@ -6,9 +6,59 @@ use vendor\Evd\Main\Viewer;
 
 class IndexController
 {
-    public function index(){
-        $message = "Главная страница";
+    public function index()
+    {
+        $events = [
+            [
+                "title" => "Бешенные деньги",
+                "genre" => "комедия",
+                "theater" =>
+                    [
+                        "title" => "Театр комедии им. Акимова",
+                        "link" => "#",
+                    ],
+                "date" => "28.05",
+                "count" => "25",
+                "price" => "800"
+            ],
+            [
+                "title" => "Без вины виноватые",
+                "genre" => "комедия",
+                "theater" =>
+                    [
+                        "title" => "Театр комедии им. Акимова",
+                        "link" => "#",
+                    ],
+                "date" => "18.04",
+                "count" => "15",
+                "price" => "200"
+            ],
+            [
+                "title" => "Мастер и Маргарита",
+                "genre" => "мюзикл",
+                "theater" =>
+                    [
+                        "title" => "ЛДМ. Новая-новая сцена",
+                        "link" => "#",
+                    ],
+                "date" => "08.04",
+                "count" => "19",
+                "price" => "350"
+            ],
+            [
+                "title" => "Приговор любви",
+                "genre" => "Алеко",
+                "theater" =>
+                    [
+                        "title" => "ЛДМ. Новая-новая сцена",
+                        "link" => "#",
+                    ],
+                "date" => "30.04",
+                "count" => "34",
+                "price" => "450"
+            ],
+        ];
 
-        Viewer::view("index",compact("message"));
+        Viewer::view("index", compact("events"));
     }
 }
--- views/index.php
+++ views/index.php
@@ -1,167 +1,53 @@
 <?php include_once "layouts/header.php" ?>
 
-<section class="events">
-    <div class="container">
-        <div class="events__inner">
-            <div class="events__card">
-                <div class="event__card__top-section">
-                    <div class="event__card__top-info">
-                        <div class="event__card__title">
-                            <a href="#">
-                                Бешенные деньги
-                            </a>
+    <section class="events">
+        <div class="container">
+            <div class="events__inner">
+                <?php /** @var array $events */
+                foreach ($events as $event) { ?>
+                    <div class="events__card">
+                        <div class="event__card__top-section">
+                            <div class="event__card__top-info">
+                                <div class="event__card__title">
+                                    <a href="#">
+                                        <?= $event['title'] ?>
+                                    </a>
+                                </div>
+                                <div class="event__card__genre">
+                                    <a href="#">
+                                        <?= $event['genre'] ?>
+                                    </a>
+                                </div>
+                                <div class="event__card__theatre">
+                                    <a href="#">
+                                        <?= $event['theater']['title'] ?>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="event__card_date">
+                                <?= $event['date'] ?>
+                            </div>
                         </div>
-                        <div class="event__card__genre">
-                            <a href="#">
-                                Комедия
-                            </a>
-                        </div>
-                        <div class="event__card__theatre">
-                            <a href="#">
-                                Театр комедии им. Акимова
-                            </a>
-                        </div>
-                    </div>
-                    <div class="event__card_date">
-                        28.05
-                    </div>
-                </div>
-                <div class="event__card__bot-section">
-                    <div class="event__card__bot-info">
-                        <div class="event__card__count">
-                            Осталось: <span>25</span>
-                        </div>
-                        <div class="event__card_price">
-                            <span>800</span>руб.
-                        </div>
-                    </div>
-                    <div class="event__card__btn__container">
-                        <a href="#" class="event__card__btn">
-                            Купить
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <div class="events__card">
-                <div class="event__card__top-section">
-                    <div class="event__card__top-info">
-                        <div class="event__card__title">
-                            <a href="#">
-                                Без вины виноватые
-                            </a>
-                        </div>
-                        <div class="event__card__genre">
-                            <a href="#">
-                                Комедия
-                            </a>
-                        </div>
-                        <div class="event__card__theatre">
-                            <a href="#">
-                                Театр комедии им. Акимова
-                            </a>
+                        <div class="event__card__bot-section">
+                            <div class="event__card__bot-info">
+                                <div class="event__card__count">
+                                    Осталось: <span><?= $event['count'] ?></span>
+                                </div>
+                                <div class="event__card_price">
+                                    <span><?= $event['price'] ?></span>руб.
+                                </div>
+                            </div>
+                            <div class="event__card__btn__container">
+                                <a href="#" class="event__card__btn">
+                                    Купить
+                                </a>
+                            </div>
                         </div>
                     </div>
-                    <div class="event__card_date">
-                        28.05
-                    </div>
-                </div>
-                <div class="event__card__bot-section">
-                    <div class="event__card__bot-info">
-                        <div class="event__card__count">
-                            Осталось: <span>25</span>
-                        </div>
-                        <div class="event__card_price">
-                            <span>800</span>руб.
-                        </div>
-                    </div>
-                    <div class="event__card__btn__container">
-                        <a href="#" class="event__card__btn">
-                            Купить
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <div class="events__card">
-                <div class="event__card__top-section">
-                    <div class="event__card__top-info">
-                        <div class="event__card__title">
-                            <a href="#">
-                                Мастер и Маргарита
-                            </a>
-                        </div>
-                        <div class="event__card__genre">
-                            <a href="#">
-                                Мюзикл
-                            </a>
-                        </div>
-                        <div class="event__card__theatre">
-                            <a href="#">
-                                ЛДМ. Новая-новая сцена
-                            </a>
-                        </div>
-                    </div>
-                    <div class="event__card_date">
-                        28.05
-                    </div>
-                </div>
-                <div class="event__card__bot-section">
-                    <div class="event__card__bot-info">
-                        <div class="event__card__count">
-                            Осталось: <span>25</span>
-                        </div>
-                        <div class="event__card_price">
-                            <span>800</span>руб.
-                        </div>
-                    </div>
-                    <div class="event__card__btn__container">
-                        <a href="#" class="event__card__btn">
-                            Купить
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <div class="events__card">
-                <div class="event__card__top-section">
-                    <div class="event__card__top-info">
-                        <div class="event__card__title">
-                            <a href="#">
-                                Приговор любви
-                            </a>
-                        </div>
-                        <div class="event__card__genre">
-                            <a href="#">
-                                Мюзикл
-                            </a>
-                        </div>
-                        <div class="event__card__theatre">
-                            <a href="#">
-                                Алеко
-                            </a>
-                        </div>
-                    </div>
-                    <div class="event__card_date">
-                        28.05
-                    </div>
-                </div>
-                <div class="event__card__bot-section">
-                    <div class="event__card__bot-info">
-                        <div class="event__card__count">
-                            Осталось: <span>25</span>
-                        </div>
-                        <div class="event__card_price">
-                            <span>800</span>руб.
-                        </div>
-                    </div>
-                    <div class="event__card__btn__container">
-                        <a href="#" class="event__card__btn">
-                            Купить
-                        </a>
-                    </div>
-                </div>
+                <?php } ?>
             </div>
         </div>
-    </div>
-</section>
+    </section>
 
 
 <?php include_once "layouts/footer.php" ?>
