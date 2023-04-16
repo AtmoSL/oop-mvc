867e7f55fd76297b627859c61134b51efd061739
Ilya <devdokimov.ilya@yandex.ru>
2023-04-17 00:40:13 +0300

    Абстрактный класс модели. Создание статического метода вывода всех элементов

---
+++ vendor/Evd/Main/Model.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace vendor\Evd\Main;
+
+
+use vendor\Evd\DataBase\DB;
+
+/**
+ * Класс модели, от которого должны наследоваться все модели.
+ */
+abstract class Model
+{
+//    abstract static $table; //Название таблицы
+    protected $fields; //поля таблицы
+
+    private $fieldsSql = "*"; // Поля для запроса
+
+    protected $where = "1";
+
+    /**
+     * Вернуть все записи в виде массива
+     * @return void
+     */
+    public static function all($fields = "*")
+    {
+        $table = static::$table;
+
+        $sql = "SELECT $fields FROM $table WHERE 1";
+
+        $stmt = DB:: query($sql);
+        $result = $stmt->fetchAll(2);
+        return $result;
+    }
+
+
+}
