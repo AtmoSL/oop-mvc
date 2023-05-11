b6667b4b44208e31e1f82b3e0daaae16fdb7497d
Ilya <devdokimov.ilya@yandex.ru>
2023-05-11 22:54:09 +0300

    Создание метода register для обработки регистрации. Пока просто выводит Post

--- app/Controllers/RegistrationController.php
+++ app/Controllers/RegistrationController.php
@@ -7,8 +7,15 @@ use vendor\Evd\Main\Viewer;
 class RegistrationController
 {
     public function index(){
-
-
         Viewer::view("registration");
+    }
+
+    /**
+     * Метод регистрации
+     * @param $data
+     * @return void
+     */
+    public function register($data){
+        debug($data);
     }
 }
